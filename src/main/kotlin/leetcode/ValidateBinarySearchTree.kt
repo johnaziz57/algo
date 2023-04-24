11fccd1d22d265c60b6f9c40a2d82574e3e316ba
John Salib <john.salib@teampicnic.com>
2023-04-25 01:05:36 +0200

    Add Validate Binary Search Tree

--- src/main/kotlin/leetcode/ValidateBinarySearchTree.kt
+++ src/main/kotlin/leetcode/ValidateBinarySearchTree.kt
@@ -2,70 +2,31 @@ package leetcode
 
 /**
  * https://leetcode.com/problems/validate-binary-search-tree/
- * WRONG SOLUTION
  */
 class ValidateBinarySearchTree {
     fun isValidBST(root: TreeNode?): Boolean {
-        return visitTree(root)
+        return root?.let { visitNode(it).first } ?: true
     }
 
-    private fun visitTree(treeNode: TreeNode?): Boolean {
-        treeNode ?: return true
-        treeNode.left?.let {
-            val leftPair = visitLeft(it)
-            if (leftPair.second.not() || leftPair.first >= treeNode.`val`) {
-                return false
-            }
-        }
-
-        treeNode.right?.let {
-            val rightPair = visitRight(it)
-            if (rightPair.second.not() || rightPair.first <=  treeNode.`val`) {
-                return false
-            }
-        }
-        return true
-    }
-
-    private fun visitLeft(treeNode: TreeNode): Pair<Int, Boolean> {
+    private fun visitNode(treeNode: TreeNode): Triple<Boolean, Int, Int> {
         val left = treeNode.left
+        var leftTriple: Triple<Boolean, Int, Int>? = null
         if (left != null) {
-            val leftPair = visitLeft(left)
-            if (leftPair.second.not() || leftPair.first >= treeNode.`val`) {
-                return -1 to false
+            leftTriple = visitNode(left)
+            if (leftTriple.first.not() || leftTriple.second >= treeNode.`val` || leftTriple.third >= treeNode.`val`) {
+                return Triple(false, 0, 0)
             }
         }
 
         val right = treeNode.right
+        var rightTriple: Triple<Boolean, Int, Int>? = null
         if (right != null) {
-            val rightPair = visitRight(right)
-            if (rightPair.second.not() || rightPair.first <=  treeNode.`val`) {
-                return -1 to false
-            }
-            return rightPair
-        }
-        return treeNode.`val` to true
-    }
-
-    private fun visitRight(treeNode: TreeNode): Pair<Int, Boolean> {
-        val right = treeNode.right
-        if (right != null) {
-            val rightPair = visitRight(right)
-            if (rightPair.second.not() || rightPair.first <=  treeNode.`val`) {
-                return -1 to false
+            rightTriple = visitNode(right)
+            if (rightTriple.first.not() || rightTriple.second <= treeNode.`val` || rightTriple.third <= treeNode.`val`) {
+                return Triple(false, 0, 0)
             }
         }
-
-        val left = treeNode.left
-        if (left != null) {
-            val leftPair = visitLeft(left)
-            if (leftPair.second.not() || leftPair.first >= treeNode.`val`) {
-                return -1 to false
-            }
-            return leftPair
-        }
-
-        return treeNode.`val` to true
+        return Triple(true, leftTriple?.second ?: treeNode.`val`, rightTriple?.third ?: treeNode.`val`)
     }
 }
 
